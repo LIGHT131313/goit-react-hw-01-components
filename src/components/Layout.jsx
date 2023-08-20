5af928b9bcbf2aa66b632dc3038ff7d0abbbc049
LIGHT131313 <131313light@gmail.com>
2023-08-20 17:33:52 +0300

    change name files

--- src/components/Layout.jsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const Layout = styled.div`
-  display: flex;
-  justify-content: center;
-  padding: 20px;
-  max-width: 1200px;
-  margin: 0 auto;
-`;
